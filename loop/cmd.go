397c52d92770224ea42d8dd0746982970cb5047f
Pedro Gomes <pedrogbranquinho@gmail.com>
2025-03-25 14:25:17 -0300

    chore: update docs to latest version

--- loop/cmd.go
+++ loop/cmd.go
@@ -23,8 +23,8 @@ func init() {
 var Cmd = &Z.Cmd{
 	Name:      `lazywal`,
 	Summary:   `Lazywal: a terminal client to facilitate setting up video-loops/gifs as background.`,
-	Version:   `v1.0.4`,
-	Copyright: `Copyright 2021-2024 Zayac-The-Engineer, 2025 Pedro G. Branquinho (Go version)`,
+	Version:   `v1.1.0`,
+	Copyright: `Copyright 2021-2024 Zayac-The-Engineer, 2024-2025 Pedro G. Branquinho (Go version)`,
 	License:   `MIT License`,
 	Site:      `buddhilw.com`,
 	Source:    `git@github.com/BuddhiLW/lazywal.git`,
